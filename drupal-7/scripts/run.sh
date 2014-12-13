a35fbab117c8de94dac62fadab80af0a3651ffcc
Timani Tunduwani <timani@users.noreply.github.com>
2014-12-13 15:30:42 -0800

    Update run.sh

---
+++ drupal-7/scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# We cannot be in a Drupal directory to run aliased drush commands.
+cd $HOME 
+pwd
+echo $HOME
+
+# Make a Drupal installation to run the tests
+drush --yes make ./drupal-7/scripts/drupal-7.make $BUILD_DIR
+mkdir -p $BUILD_DIR/$MODULE_PATH
+cd $BUILD_DIR 
+pwd
+
+# - git archive $(git rev-parse --abbrev-ref HEAD) | tar -x -C $MODULE_PATH
+# Install Drupal
+drush --yes site-install minimal --db-url="$DATABASE_URL" 
+drush --yes en simpletest
